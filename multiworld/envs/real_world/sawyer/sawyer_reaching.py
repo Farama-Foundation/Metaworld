76c90a3162f3f54c5732fcf2f2a4afc1ffd106c0
Murtaza Dalal <mdalal@berkeley.edu>
2018-07-13 18:05:42 -0700

    have reacher subclass the reacher from sawyer_control

--- multiworld/envs/real_world/sawyer/sawyer_reaching.py
+++ multiworld/envs/real_world/sawyer/sawyer_reaching.py
@@ -1,28 +1,17 @@
 from collections import OrderedDict
 import numpy as np
-from sawyer_control.envs.sawyer_env_base import SawyerEnvBase
+import sawyer_control.envs.sawyer_reaching as sawyer_reaching
 from multiworld.core.serializable import Serializable
 from multiworld.envs.env_util import get_stat_in_paths, \
     create_stats_ordered_dict
-from multiworld.core.multitask_env import MultitaskEnv
 from gym.spaces import Dict
 
-class SawyerReachXYZEnv(SawyerEnvBase, MultitaskEnv):
+class SawyerReachXYZEnv(sawyer_reaching.SawyerReachXYZEnv):
     def __init__(self,
-                 fixed_goal=(1, 1, 1),
-                 indicator_threshold=.05,
-                 reward_type='hand_distance',
                  **kwargs
                  ):
         Serializable.quick_init(self, locals())
-        SawyerEnvBase.__init__(self, **kwargs)
-        if self.action_mode=='torque':
-            self.goal_space = self.config.TORQUE_SAFETY_BOX
-        else:
-            self.goal_space = self.config.POSITION_SAFETY_BOX
-        self.indicator_threshold=indicator_threshold
-        self.reward_type = reward_type
-        self._state_goal = np.array(fixed_goal)
+        super().__init__(self, **kwargs)
         self.observation_space = Dict([
             ('observation', self._observation_space),
             ('desired_goal', self.goal_space),
@@ -68,46 +57,15 @@ class SawyerReachXYZEnv(SawyerEnvBase, MultitaskEnv):
             state_achieved_goal=ee_pos,
         )
 
-    def _get_info(self):
-        hand_distance = np.linalg.norm(self._state_goal - self._get_endeffector_pose())
-        return dict(
-            hand_distance=hand_distance,
-            hand_success=(hand_distance<self.indicator_threshold).astype(float)
-        )
-
     def reset(self):
         self._reset_robot()
         goal = self.sample_goal()
         self._state_goal = goal['state_desired_goal']
         return self._get_obs()
 
-    def get_diagnostics(self, paths, prefix=''):
-        statistics = OrderedDict()
-        for stat_name in [
-            'hand_distance',
-            'hand_success',
-        ]:
-            stat_name = stat_name
-            stat = get_stat_in_paths(paths, 'env_infos', stat_name)
-            statistics.update(create_stats_ordered_dict(
-                '%s%s' % (prefix, stat_name),
-                stat,
-                always_show_all_stats=True,
-                ))
-            statistics.update(create_stats_ordered_dict(
-                'Final %s%s' % (prefix, stat_name),
-                [s[-1] for s in stat],
-                always_show_all_stats=True,
-                ))
-        return statistics
-
     """
     Multitask functions
     """
-
-    @property
-    def goal_dim(self) -> int:
-        return 3
 
     def get_goal(self):
         return {
@@ -116,18 +74,7 @@ class SawyerReachXYZEnv(SawyerEnvBase, MultitaskEnv):
         }
 
     def sample_goals(self, batch_size):
-        if self.fix_goal:
-            goals = np.repeat(
-                self.fixed_goal.copy()[None],
-                batch_size,
-                0
-            )
-        else:
-            goals = np.random.uniform(
-                self.goal_space.low,
-                self.goal_space.high,
-                size=(batch_size, self.goal_space.low.size),
-            )
+        goals = super().sample_goals()
         return {
             'desired_goal': goals,
             'state_desired_goal': goals,
@@ -135,7 +82,4 @@ class SawyerReachXYZEnv(SawyerEnvBase, MultitaskEnv):
 
     def set_to_goal(self, goal):
         goal = goal['state_desired_goal']
-        if self.action_mode=='position':
-            self._position_act(goal - self._get_endeffector_pose()[:3])
-        else:
-            raise NotImplementedError()
+        super().set_to_goal(goal)
