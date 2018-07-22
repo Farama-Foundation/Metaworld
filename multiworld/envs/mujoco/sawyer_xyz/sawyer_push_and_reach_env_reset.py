230db3c308014b2da21831f8989b9d1ea2899a55
Murtaza Dalal <mdalal@berkeley.edu>
2018-07-22 15:49:16 -0700

    removed unnecesarry code

--- multiworld/envs/mujoco/sawyer_xyz/sawyer_push_and_reach_env_reset.py
+++ multiworld/envs/mujoco/sawyer_xyz/sawyer_push_and_reach_env_reset.py
@@ -93,8 +93,7 @@ class SawyerPushAndReachXYZEnv(MultitaskEnv, SawyerXYZEnv):
         ])
         self.init_puck_z = init_puck_z
         self.reset_free = reset_free
-        self.puck_pos = self.sample_puck_xy()
-        self._set_puck_xy(self.puck_pos)
+        self.puck_pos = self.get_puck_pos()[:2]
         self.mode(mode)
 
     def mode(self, name):
@@ -198,7 +197,6 @@ class SawyerPushAndReachXYZEnv(MultitaskEnv, SawyerXYZEnv):
         qpos[7:10] = np.hstack((pos.copy(), np.array([self.init_puck_z])))
         # qpos[10:14] = np.array([1, 0, 0, 0])
         qvel[7:14] = 0
-        self.puck_pos = pos
         self.set_state(qpos, qvel)
 
     def reset_model(self):
