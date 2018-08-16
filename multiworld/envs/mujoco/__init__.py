dbd4c85e6201abd77ed38f0555acf6454b86c60f
Vitchyr Pong <vitchyr@gmail.com>
2018-08-16 14:43:43 -0700

    add state distance reset free push and reach

--- multiworld/envs/mujoco/__init__.py
+++ multiworld/envs/mujoco/__init__.py
@@ -59,6 +59,24 @@ def register_custom_envs():
             goal_high=(0.25, 0.875, 0.02, .2, .8),
         )
     )
+    register(
+        id='SawyerPushAndReacherXYEnv-StateDistance-ResetFree-v0',
+        entry_point='multiworld.envs.mujoco.sawyer_xyz.sawyer_push_and_reach_env:SawyerPushAndReachXYEnv',
+        tags={
+            'git-commit-hash': '3d4adbe',
+            'author': 'vitchyr'
+        },
+        kwargs=dict(
+            reward_type='state_distance',
+            reset_free=True,
+            hand_low=(-0.28, 0.3, 0.05),
+            hand_high=(0.28, 0.9, 0.3),
+            puck_low=(-.4, .2),
+            puck_high=(.4, 1),
+            goal_low=(-0.25, 0.3, 0.02, -.2, .4),
+            goal_high=(0.25, 0.875, 0.02, .2, .8),
+        )
+    )
 
 
 def create_image_48_sawyer_reach_xy_env_v0():
