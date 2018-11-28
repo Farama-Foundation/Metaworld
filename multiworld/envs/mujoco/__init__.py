22309499d949c9cf15c9066f931a6a201b00ee05
Ashvin Nair <nair.ashvin@gmail.com>
2018-11-27 23:11:54 -0800

    Rearrange __init__

--- multiworld/envs/mujoco/__init__.py
+++ multiworld/envs/mujoco/__init__.py
@@ -52,7 +52,6 @@ def register_custom_envs():
 
     """
     Pushing Tasks, XY
-
     """
 
     register(
@@ -218,6 +217,26 @@ def register_custom_envs():
     )
 
     """
+    NIPS submission pusher environment
+    """
+    register(
+        id='SawyerPushNIPS-v0',
+        entry_point='multiworld.envs.mujoco.sawyer_xyz'
+                    '.sawyer_push_nips:SawyerPushAndReachXYEasyEnv',
+        tags={
+            'git-commit-hash': 'bede25d',
+            'author': 'ashvin',
+        },
+        kwargs=dict(
+            hide_goal=True,
+            reward_info=dict(
+                type="state_distance",
+            ),
+        )
+
+    )
+
+    """
     Door Hook Env
     """
 
@@ -278,26 +297,6 @@ def register_custom_envs():
             action_scale=0.02,
             hide_goal_markers=True,
             num_goals_presampled=1000,
-        )
-
-    )
-
-    """
-    NIPS submission pusher environment
-    """
-    register(
-        id='SawyerPushNIPS-v0',
-        entry_point='multiworld.envs.mujoco.sawyer_xyz'
-                    '.sawyer_push_nips:SawyerPushAndReachXYEasyEnv',
-        tags={
-            'git-commit-hash': 'bede25d',
-            'author': 'ashvin',
-        },
-        kwargs=dict(
-            hide_goal=True,
-            reward_info=dict(
-                type="state_distance",
-            ),
         )
 
     )
