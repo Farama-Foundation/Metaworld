9042f575613dcd5a6a541d1fc07bde10147e78e2
Murtaza Dalal <mdalal@berkeley.edu>
2018-12-29 17:22:14 -0800

    added registered reacher env v1 (fixed)

--- multiworld/envs/mujoco/__init__.py
+++ multiworld/envs/mujoco/__init__.py
@@ -46,6 +46,19 @@ def register_custom_envs():
     )
 
     register(
+        id='SawyerReachXYZEnv-v1',
+        entry_point='multiworld.envs.mujoco.sawyer_xyz.sawyer_reach:SawyerReachXYZEnv',
+        tags={
+            'git-commit-hash': 'bea5de',
+            'author': 'murtaza'
+        },
+        kwargs={
+            'hide_goal_markers': True,
+            'norm_order': 2,
+        },
+    )
+
+    register(
         id='Image48SawyerReachXYEnv-v1',
         entry_point=create_image_48_sawyer_reach_xy_env_v1,
         tags={
