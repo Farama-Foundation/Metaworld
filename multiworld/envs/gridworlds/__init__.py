fa30c786dd1bd547207534c1d352a2718a875424
Coline Devin <coline@berkeley.edu>
2019-02-05 17:01:40 -0800

    Small fixes

--- multiworld/envs/gridworlds/__init__.py
+++ multiworld/envs/gridworlds/__init__.py
@@ -1,11 +1,10 @@
 from gym.envs.registration import register
-
 register(
     id='GoalGridworld-v0',
-    entry_point='rlkit.envs.goal_gridworld:GoalGridworld',
+    entry_point='multiworld.envs.gridworlds.goal_gridworld:GoalGridworld',
 )
 register(
     id='GoalGridworld-Concatenated-v0',
-    entry_point='rlkit.envs.goal_gridworld:GoalGridworld',
+    entry_point='multiworld.envs.gridworlds.goal_gridworld:GoalGridworld',
      kwargs={'concatenated':True}
 )
