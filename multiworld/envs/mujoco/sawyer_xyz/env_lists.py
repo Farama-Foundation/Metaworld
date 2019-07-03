c936d25ce1784feaa38291bcd019619b0fe0634e
Tianhe <tianheyu927@gmail.com>
2019-07-03 13:50:46 -0700

    added hard mode

--- multiworld/envs/mujoco/sawyer_xyz/env_lists.py
+++ multiworld/envs/mujoco/sawyer_xyz/env_lists.py
@@ -19,6 +19,47 @@ from multiworld.envs.mujoco.sawyer_xyz.sawyer_drawer_close_6dof import SawyerDra
 from multiworld.envs.mujoco.sawyer_xyz.sawyer_box_open_6dof import SawyerBoxOpen6DOFEnv
 from multiworld.envs.mujoco.sawyer_xyz.sawyer_box_close_6dof import SawyerBoxClose6DOFEnv
 from multiworld.envs.mujoco.sawyer_xyz.sawyer_peg_insertion_side_6dof import SawyerPegInsertionSide6DOFEnv
+from multiworld.envs.mujoco.sawyer_xyz.sawyer_bin_picking_6dof import SawyerBinPicking6DOFEnv
+from multiworld.envs.mujoco.sawyer_xyz.sawyer_drawer_close_6dof import SawyerDrawerClose6DOFEnv
+from multiworld.envs.mujoco.sawyer_xyz.sawyer_laptop_close_6dof import SawyerLaptopClose6DOFEnv
+from multiworld.envs.mujoco.sawyer_xyz.sawyer_box_close_6dof import SawyerBoxClose6DOFEnv
+from multiworld.envs.mujoco.sawyer_xyz.sawyer_stick_push_6dof import SawyerStickPush6DOFEnv
+from multiworld.envs.mujoco.sawyer_xyz.sawyer_stick_pull_6dof import SawyerStickPull6DOFEnv
+from multiworld.envs.mujoco.sawyer_xyz.sawyer_button_press_6dof import SawyerButtonPress6DOFEnv
+from multiworld.envs.mujoco.sawyer_xyz.sawyer_shelf_place_6dof import SawyerShelfPlace6DOFEnv
+from multiworld.envs.mujoco.sawyer_xyz.sawyer_door_close import SawyerDoorClose6DOFEnv
+from multiworld.envs.mujoco.sawyer_xyz.sawyer_sweep_into_goal import SawyerSweepIntoGoal6DOFEnv
+from multiworld.envs.mujoco.sawyer_xyz.sawyer_coffee_button_6dof import SawyerCoffeeButton6DOFEnv
+from multiworld.envs.mujoco.sawyer_xyz.sawyer_coffee_push_6dof import SawyerCoffeePush6DOFEnv
+from multiworld.envs.mujoco.sawyer_xyz.sawyer_coffee_pull_6dof import SawyerCoffeePull6DOFEnv
+from multiworld.envs.mujoco.sawyer_xyz.sawyer_faucet_open import SawyerFaucetOpen6DOFEnv
+from multiworld.envs.mujoco.sawyer_xyz.sawyer_faucet_close import SawyerFaucetClose6DOFEnv
+from multiworld.envs.mujoco.sawyer_xyz.sawyer_peg_insertion_topdown_6dof import SawyerPegInsertionTopdown6DOFEnv
+from multiworld.envs.mujoco.sawyer_xyz.sawyer_peg_unplug_topdown_6dof import SawyerPegUnplugTopdown6DOFEnv
+from multiworld.envs.mujoco.sawyer_xyz.sawyer_peg_unplug_side_6dof import SawyerPegUnplugSide6DOFEnv
+from multiworld.envs.mujoco.sawyer_xyz.sawyer_soccer import SawyerSoccer6DOFEnv
+from multiworld.envs.mujoco.sawyer_xyz.sawyer_basketball import SawyerBasketball6DOFEnv
+from multiworld.envs.mujoco.sawyer_xyz.sawyer_reach_push_pick_place_wall_6dof import SawyerReachPushPickPlaceWall6DOFEnv
+from multiworld.envs.mujoco.sawyer_xyz.sawyer_unstack_6dof import SawyerUnStack6DOFEnv
+from multiworld.envs.mujoco.sawyer_xyz.sawyer_push_back_6dof import SawyerPushBack6DOFEnv
+from multiworld.envs.mujoco.sawyer_xyz.sawyer_pick_out_of_hole import SawyerPickOutOfHole6DOFEnv
+from multiworld.envs.mujoco.sawyer_xyz.sawyer_shelf_remove_6dof import SawyerShelfRemove6DOFEnv
+from multiworld.envs.mujoco.sawyer_xyz.sawyer_shelf_remove_front_6dof import SawyerShelfRemoveFront6DOFEnv
+from multiworld.envs.mujoco.sawyer_xyz.sawyer_disassemble_peg_6dof import SawyerNutDisassemble6DOFEnv
+from multiworld.envs.mujoco.sawyer_xyz.sawyer_door_lock import SawyerDoorLock6DOFEnv
+from multiworld.envs.mujoco.sawyer_xyz.sawyer_door_unlock import SawyerDoorUnlock6DOFEnv
+from multiworld.envs.mujoco.sawyer_xyz.sawyer_sweep_tool import SawyerSweepTool6DOFEnv
+from multiworld.envs.mujoco.sawyer_xyz.sawyer_golf_putting import SawyerGolfPutting6DOFEnv
+from multiworld.envs.mujoco.sawyer_xyz.sawyer_button_press_wall_6dof import SawyerButtonPressWall6DOFEnv
+from multiworld.envs.mujoco.sawyer_xyz.sawyer_button_press_topdown_wall_6dof import SawyerButtonPressTopdownWall6DOFEnv
+from multiworld.envs.mujoco.sawyer_xyz.sawyer_handle_press_6dof import SawyerHandlePress6DOFEnv
+from multiworld.envs.mujoco.sawyer_xyz.sawyer_handle_pull_6dof import SawyerHandlePull6DOFEnv
+from multiworld.envs.mujoco.sawyer_xyz.sawyer_handle_press_side_6dof import SawyerHandlePressSide6DOFEnv
+from multiworld.envs.mujoco.sawyer_xyz.sawyer_handle_pull_side_6dof import SawyerHandlePullSide6DOFEnv
+from multiworld.envs.mujoco.sawyer_xyz.sawyer_plate_slide_6dof import SawyerPlateSlide6DOFEnv
+from multiworld.envs.mujoco.sawyer_xyz.sawyer_plate_slide_back_6dof import SawyerPlateSlideBack6DOFEnv
+from multiworld.envs.mujoco.sawyer_xyz.sawyer_plate_slide_side_6dof import SawyerPlateSlideSide6DOFEnv
+from multiworld.envs.mujoco.sawyer_xyz.sawyer_plate_slide_back_side_6dof import SawyerPlateSlideBackSide6DOFEnv
 
 
 # easy mode for CoRL
@@ -31,6 +72,60 @@ EASY_MODE_LIST = [
     SawyerPegInsertionSide6DOFEnv,
     SawyerWindowOpen6DOFEnv,
     SawyerWindowClose6DOFEnv,
+]
+
+# hard mode for CoRL
+HARD_MODE_LIST = [
+    SawyerReachPushPickPlace6DOFEnv,
+    SawyerReachPushPickPlace6DOFEnv,
+    SawyerReachPushPickPlace6DOFEnv,
+    SawyerReachPushPickPlaceWall6DOFEnv,
+    SawyerReachPushPickPlaceWall6DOFEnv,
+    SawyerReachPushPickPlaceWall6DOFEnv,
+    SawyerDoor6DOFEnv,
+    SawyerDoorClose6DOFEnv,
+    SawyerDrawerOpen6DOFEnv,
+    SawyerDrawerClose6DOFEnv,
+    SawyerButtonPressTopdown6DOFEnv,
+    SawyerButtonPress6DOFEnv,
+    SawyerButtonPressTopdownWall6DOFEnv,
+    SawyerButtonPressWall6DOFEnv,
+    SawyerPegInsertionSide6DOFEnv,
+    SawyerPegUnplugSide6DOFEnv,
+    SawyerWindowOpen6DOFEnv,
+    SawyerWindowClose6DOFEnv,
+    SawyerNutDisassemble6DOFEnv,
+    SawyerHammer6DOFEnv,
+    SawyerPlateSlide6DOFEnv,
+    SawyerPlateSlideSide6DOFEnv,
+    SawyerPlateSlideBack6DOFEnv, 
+    SawyerPlateSlideBackSide6DOFEnv,
+    SawyerHandlePress6DOFEnv,
+    SawyerHandlePull6DOFEnv,
+    SawyerHandlePressSide6DOFEnv,
+    SawyerHandlePullSide6DOFEnv,
+    SawyerStickPush6DOFEnv,
+    SawyerStickPull6DOFEnv,
+    SawyerBasketball6DOFEnv,
+    SawyerSoccer6DOFEnv,
+    SawyerFaucetOpen6DOFEnv,
+    SawyerFaucetClose6DOFEnv,
+    SawyerCoffeePush6DOFEnv,
+    SawyerCoffeePull6DOFEnv,
+    SawyerCoffeeButton6DOFEnv,
+    SawyerSweep6DOFEnv,
+    SawyerSweepIntoGoal6DOFEnv,
+    SawyerStack6DOFEnv,
+    SawyerUnStack6DOFEnv,
+    SawyerShelfPlace6DOFEnv,
+    SawyerLaptopClose6DOFEnv,
+    SawyerLeverPull6DOFEnv,
+    SawyerDialTurn6DOFEnv,
+    SawyerBinPicking6DOFEnv,
+    SawyerBoxOpen6DOFEnv,
+    SawyerBoxClose6DOFEnv,
+    SawyerDoorLock6DOFEnv,
+    SawyerDoorUnlock6DOFEnv
 ]
 
 
