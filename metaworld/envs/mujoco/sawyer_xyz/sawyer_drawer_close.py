dfdbc7cf495678ee96b360d1e6e199acc141b36c
Tianhe Yu <tianheyu927@gmail.com>
2020-01-10 11:23:34 -0800

    drawer_close bugfix (#37)

--- metaworld/envs/mujoco/sawyer_xyz/sawyer_drawer_close.py
+++ metaworld/envs/mujoco/sawyer_xyz/sawyer_drawer_close.py
@@ -43,7 +43,7 @@ class SawyerDrawerCloseEnv(SawyerXYZEnv):
             'obj_init_pos': np.array([0., 0.9, 0.04], dtype=np.float32),
             'hand_init_pos': np.array([0, 0.6, 0.2], dtype=np.float32),
         }
-        self.goal = np.array([0., 0.55, 0.04])
+        self.goal = np.array([0., 0.7, 0.04])
         self.obj_init_pos = self.init_config['obj_init_pos']
         self.obj_init_angle = self.init_config['obj_init_angle']
         self.hand_init_pos = self.init_config['hand_init_pos']
