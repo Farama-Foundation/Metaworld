931aae86825fc0d86a1226a70de611f4a38fb6f5
Ryan Julian <ryanjulian@users.noreply.github.com>
2020-01-23 15:43:23 -0800

    Make gym dependency more permissive

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 # Required dependencies
 required = [
     # Please keep alphabetized
-    'gym==0.12.1',
+    'gym>=0.15.4',
     'mujoco-py<2.1,>=2.0',
     'opencv-python>=4.1.0.25',
 ]
