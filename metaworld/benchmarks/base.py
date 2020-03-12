68696cc725c65a461c7aa7c2dda8b17e7d08413c
Keren Zhu <naeioi@hotmail.com>
2020-03-12 12:20:17 -0700

    Better support for meta-RL training
    * Add new class methods to construct a MultiClassMultiTaskEnv with only one task
    * Pad observation according to maximum observation dimension among all tasks

--- metaworld/benchmarks/base.py
+++ metaworld/benchmarks/base.py
@@ -8,3 +8,19 @@ class Benchmark:
     @classmethod
     def get_test_tasks(cls, sample_all=False):
         return cls(env_type='test', sample_all=sample_all)
+
+    @classmethod
+    def from_task(cls, task_name):
+        """Construct a Benchmark instance with one task.
+
+        Args:
+            cls (metaworld.benchmarks.Benchmark): Class of the instance.
+            task_name (str): Task name. Subclasses should check if the
+                task exists, raise ValueError if not.
+
+        Returns:
+            metaworld.benchmarks.Benchmark: Instance that contains
+                the required task.
+
+        """
+        raise NotImplementedError
--- metaworld/benchmarks/ml45.py
+++ metaworld/benchmarks/ml45.py
@@ -6,11 +6,18 @@ from metaworld.envs.mujoco.env_dict import HARD_MODE_ARGS_KWARGS, HARD_MODE_CLS_
 
 class ML45(MultiClassMultiTaskEnv, Benchmark):
 
-    def __init__(self, env_type='train', sample_all=False):
+    def __init__(self, env_type='train', sample_all=False, task_name=None):
         assert env_type == 'train' or env_type == 'test'
 
-        cls_dict = HARD_MODE_CLS_DICT[env_type]
-        args_kwargs = HARD_MODE_ARGS_KWARGS[env_type]
+        if task_name is not None:
+            if task_name not in HARD_MODE_CLS_DICT[env_type]:
+                raise ValueError("{} does not exist in ML45 {} tasks".format(
+                    task_name, env_type))
+            cls_dict = {task_name: HARD_MODE_CLS_DICT[env_type][task_name]}
+            args_kwargs = {task_name: HARD_MODE_ARGS_KWARGS[env_type][task_name]}
+        else:
+            cls_dict = HARD_MODE_CLS_DICT[env_type]
+            args_kwargs = HARD_MODE_ARGS_KWARGS[env_type]
 
         super().__init__(
             task_env_cls_dict=cls_dict,
@@ -18,3 +25,12 @@ class ML45(MultiClassMultiTaskEnv, Benchmark):
             sample_goals=True,
             obs_type='plain',
             sample_all=sample_all)
+
+    @classmethod
+    def from_task(cls, task_name):
+        if task_name in HARD_MODE_CLS_DICT['train']:
+            return cls(env_type='train', sample_all=True, task_name=task_name)
+        elif task_name in HARD_MODE_CLS_DICT['test']:
+            return cls(env_type='test', sample_all=True, task_name=task_name)
+        else:
+            raise ValueError('{} does not exist in ML45'.format(task_name))
