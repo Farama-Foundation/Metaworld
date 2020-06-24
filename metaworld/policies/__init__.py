ad78a25dd022fc544ae66fdbdca4e68b02463853
Adithya Bellathur <bellathu@usc.edu>
2020-06-24 15:47:45 -0700

    Create Pick-Place-Wall-V2 Environment and Scripted Policy
        * Created new SawyerPickPlaceWallV2 environment (with assets file)
    with new observation space with vector to end goal. Reach Wall is now
    split from Reach Push Pick Place Wall.
        * Added Scripted Policy for Pick Place Wall V2. Policy succeeds at
    an ~99% success rate with action_noise_pct=0.0, and ~96% success rate
    with action_noise_pct=0.1

--- metaworld/policies/__init__.py
+++ metaworld/policies/__init__.py
@@ -1,7 +1,9 @@
 from metaworld.policies.sawyer_window_open_v2_policy import SawyerWindowOpenV2Policy
 from metaworld.policies.sawyer_window_close_v2_policy import SawyerWindowCloseV2Policy
+from metaworld.policies.sawyer_pick_place_wall_v2_policy import SawyerPickPlaceWallV2Policy
 
 __all__ = [
     'SawyerWindowOpenV2Policy',
-    'SawyerWindowCloseV2Policy'
+    'SawyerWindowCloseV2Policy',
+    'SawyerPickPlaceWallV2Policy'
 ]
