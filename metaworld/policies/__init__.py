39e885c992587497470fca493c79bfba42fc0498
Hayden Shively <17186559+haydenshively@users.noreply.github.com>
2020-06-24 13:59:59 -0500

    Finished scripted policy for basketball env (#106)
    Tested this PR manually. Passes all tests.

--- metaworld/policies/__init__.py
+++ metaworld/policies/__init__.py
@@ -8,6 +8,7 @@ from metaworld.policies.sawyer_drawer_close_v1_policy import SawyerDrawerCloseV1
 from metaworld.policies.sawyer_pick_place_v2_policy import SawyerPickPlaceV2Policy
 from metaworld.policies.sawyer_push_v2_policy import SawyerPushV2Policy
 from metaworld.policies.sawyer_reach_v2_policy import SawyerReachV2Policy
+from metaworld.policies.sawyer_basketball_v2_policy import SawyerBasketballV2Policy
 from metaworld.policies.sawyer_peg_insertion_side_v2_policy import SawyerPegInsertionSideV2Policy
 from metaworld.policies.sawyer_peg_unplug_side_v1_policy import SawyerPegUnplugSideV1Policy
 from metaworld.policies.sawyer_window_open_v2_policy import SawyerWindowOpenV2Policy
@@ -24,6 +25,7 @@ __all__ = [
     'SawyerPickPlaceV2Policy',
     'SawyerPushV2Policy',
     'SawyerReachV2Policy',
+    'SawyerBasketballV2Policy',
     'SawyerPegInsertionSideV2Policy',
     'SawyerPegUnplugSideV1Policy',
     'SawyerWindowOpenV2Policy',
