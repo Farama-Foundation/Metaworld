16fb1d562c65b0d6caf70be1dbad24f9219bda61
Hayden Shively <haydenshively@gmail.com>
2020-07-03 16:25:47 -0500

    Separate trajectory gen from check_success()

--- tests/metaworld/envs/mujoco/sawyer_xyz/utils.py
+++ tests/metaworld/envs/mujoco/sawyer_xyz/utils.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def check_success(env, policy, act_noise_pct, render=False):
+def check_success(env, policy, act_noise_pct, render=False, end_on_success=True):
     """Tests whether a given policy solves an environment
     Args:
         env (metaworld.envs.MujocoEnv): Environment to test
@@ -10,8 +10,41 @@ def check_success(env, policy, act_noise_pct, render=False):
         act_noise_pct (float): Decimal value indicating std deviation of the
             noise as a % of action space
         render (bool): Whether to render the env in a GUI
+        end_on_success (bool): Whether to stop stepping after first success
     Returns:
-        (bool, int): Success flag, Trajectory length
+        (bool, int, np.ndarray, np.ndarray, bool): Success flag,
+            Trajectory length, Rewards, Returns, Index of first success
+    """
+    success = False
+    first_success = 0
+    rewards = []
+
+    for t, (r, done, info) in enumerate(trajectory_generator(env, policy, act_noise_pct, render)):
+        rewards.append(r)
+
+        success |= bool(info['success'])
+        if not success:
+            first_success = t
+        if (success or done) and end_on_success:
+            break
+
+    rewards = np.array(rewards)
+    returns = np.cumsum(rewards)
+
+    return success, t, rewards, returns, first_success
+
+
+def trajectory_generator(env, policy, act_noise_pct, render=False):
+    """Tests whether a given policy solves an environment
+    Args:
+        env (metaworld.envs.MujocoEnv): Environment to test
+        policy (metaworld.policies.policies.Policy): Policy that's supposed to
+            succeed in env
+        act_noise_pct (float): Decimal value indicating std deviation of the
+            noise as a % of action space
+        render (bool): Whether to render the env in a GUI
+    Yields:
+        (float, bool, dict): Reward, Done flag, Info dictionary
     """
     action_space_ptp = env.action_space.high - env.action_space.low
 
@@ -20,21 +53,12 @@ def check_success(env, policy, act_noise_pct, render=False):
     o = env.reset()
     assert o.shape == env.observation_space.shape
 
-    t = 0
-    done = False
-    success = False
-    while not success and not done:
+    for _ in range(env.max_path_length):
         a = policy.get_action(o)
         a = np.random.normal(a, act_noise_pct * action_space_ptp)
-        try:
-            o, r, done, info = env.step(a)
-            if render:
-                env.render()
 
-            t += 1
-            success |= bool(info['success'])
+        o, r, done, info = env.step(a)
+        if render:
+            env.render()
 
-        except ValueError:
-            break
-
-    return success, t
+        yield r, done, info
