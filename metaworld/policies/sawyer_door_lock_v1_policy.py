1b3df642e5f0a95c3d565b05dbd56a1abd1b40e0
Hayden Shively <haydenshively@gmail.com>
2020-07-14 18:10:28 -0500

    Update door policies to match new api

--- metaworld/policies/sawyer_door_lock_v1_policy.py
+++ metaworld/policies/sawyer_door_lock_v1_policy.py
@@ -10,8 +10,9 @@ class SawyerDoorLockV1Policy(Policy):
     @assert_fully_parsed
     def _parse_obs(obs):
         return {
-            'hand_xyz': obs[:3],
-            'lock_xyz': obs[3:],
+            'hand_pos': obs[:3],
+            'lock_pos': obs[3:6],
+            'extra_info': obs[6:],
         }
 
     def get_action(self, obs):
@@ -19,18 +20,18 @@ class SawyerDoorLockV1Policy(Policy):
 
         action = Action({
             'delta_pos': np.arange(3),
-            'grab_pow': 3
+            'grab_effort': 3
         })
 
-        action['delta_pos'] = move(o_d['hand_xyz'], to_xyz=self._desired_xyz(o_d), p=25.)
-        action['grab_pow'] = -1.
+        action['delta_pos'] = move(o_d['hand_pos'], to_xyz=self._desired_pos(o_d), p=25.)
+        action['grab_effort'] = -1.
 
         return action.array
 
     @staticmethod
-    def _desired_xyz(o_d):
-        pos_curr = o_d['hand_xyz']
-        pos_lock = o_d['lock_xyz'] + np.array([.0, -.05, .0])
+    def _desired_pos(o_d):
+        pos_curr = o_d['hand_pos']
+        pos_lock = o_d['lock_pos'] + np.array([.0, -.05, .0])
 
         if np.linalg.norm(pos_curr[:2] - pos_lock[:2]) > 0.04:
             return pos_lock + np.array([0., 0., 0.3])
