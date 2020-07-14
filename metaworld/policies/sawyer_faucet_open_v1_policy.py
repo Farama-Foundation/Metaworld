02c32727c22c0c52dba9e6348f56a82123ecd0c6
Hayden Shively <haydenshively@gmail.com>
2020-07-14 18:17:35 -0500

    Update faucet policies to match new api

--- metaworld/policies/sawyer_faucet_open_v1_policy.py
+++ metaworld/policies/sawyer_faucet_open_v1_policy.py
@@ -10,8 +10,9 @@ class SawyerFaucetOpenV1Policy(Policy):
     @assert_fully_parsed
     def _parse_obs(obs):
         return {
-            'hand_xyz': obs[:3],
-            'faucet_xyz': obs[3:],
+            'hand_pos': obs[:3],
+            'faucet_pos': obs[3:6],
+            'extra_info': obs[6:],
         }
 
     def get_action(self, obs):
@@ -19,18 +20,18 @@ class SawyerFaucetOpenV1Policy(Policy):
 
         action = Action({
             'delta_pos': np.arange(3),
-            'grab_pow': 3
+            'grab_effort': 3
         })
 
-        action['delta_pos'] = move(o_d['hand_xyz'], to_xyz=self._desired_xyz(o_d), p=25.)
-        action['grab_pow'] = 1.
+        action['delta_pos'] = move(o_d['hand_pos'], to_xyz=self._desired_pos(o_d), p=25.)
+        action['grab_effort'] = 1.
 
         return action.array
 
     @staticmethod
-    def _desired_xyz(o_d):
-        pos_curr = o_d['hand_xyz']
-        pos_faucet = o_d['faucet_xyz'] + np.array([-.02, .0, .0])
+    def _desired_pos(o_d):
+        pos_curr = o_d['hand_pos']
+        pos_faucet = o_d['faucet_pos'] + np.array([-.02, .0, .0])
 
         if np.linalg.norm(pos_curr[:2] - pos_faucet[:2]) > 0.04:
             return pos_faucet + np.array([.0, .0, .1])
