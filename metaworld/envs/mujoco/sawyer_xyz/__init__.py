20d94c728472ee10723ca2e55d82e90d337f0317
Hayden Shively <haydenshively@gmail.com>
2020-08-12 14:59:57 -0500

    Copy v1 to v2

--- metaworld/envs/mujoco/sawyer_xyz/__init__.py
+++ metaworld/envs/mujoco/sawyer_xyz/__init__.py
@@ -58,7 +58,9 @@ from metaworld.envs.mujoco.sawyer_xyz.sawyer_reach_wall_v2 import SawyerReachWal
 from metaworld.envs.mujoco.sawyer_xyz.sawyer_shelf_place import SawyerShelfPlaceEnv
 from metaworld.envs.mujoco.sawyer_xyz.sawyer_soccer import SawyerSoccerEnv
 from metaworld.envs.mujoco.sawyer_xyz.sawyer_stick_pull import SawyerStickPullEnv
+from metaworld.envs.mujoco.sawyer_xyz.sawyer_stick_pull_v2 import SawyerStickPullEnvV2
 from metaworld.envs.mujoco.sawyer_xyz.sawyer_stick_push import SawyerStickPushEnv
+from metaworld.envs.mujoco.sawyer_xyz.sawyer_stick_push_v2 import SawyerStickPushEnvV2
 from metaworld.envs.mujoco.sawyer_xyz.sawyer_sweep import SawyerSweepEnv
 from metaworld.envs.mujoco.sawyer_xyz.sawyer_sweep_into_goal import SawyerSweepIntoGoalEnv
 from metaworld.envs.mujoco.sawyer_xyz.sawyer_window_close import SawyerWindowCloseEnv
@@ -127,7 +129,9 @@ __all__ = [
     'SawyerShelfPlaceEnv',
     'SawyerSoccerEnv',
     'SawyerStickPullEnv',
+    'SawyerStickPullEnvV2',
     'SawyerStickPushEnv',
+    'SawyerStickPushEnvV2',
     'SawyerSweepEnv',
     'SawyerSweepIntoGoalEnv',
     'SawyerWindowCloseEnv',
