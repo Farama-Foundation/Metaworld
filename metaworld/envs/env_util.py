bf490342436dd466339e5599be6c2c692fef3bd5
Hayden Shively <haydenshively@gmail.com>
2020-09-01 19:12:09 -0500

    Split asset path function

--- metaworld/envs/env_util.py
+++
@@ -1,9 +0,0 @@
-import os
-
-ENV_ASSET_DIR_V1 = os.path.join(os.path.dirname(__file__), 'assets_v1')
-ENV_ASSET_DIR_V2 = os.path.join(os.path.dirname(__file__), 'assets_v2')
-
-
-def get_asset_full_path(file_name, v2=False):
-    asset_dir = ENV_ASSET_DIR_V2 if v2 else ENV_ASSET_DIR_V1
-    return os.path.join(asset_dir, file_name)
--- metaworld/envs/mujoco/sawyer_xyz/v2/sawyer_coffee_button_v2.py
+++ metaworld/envs/mujoco/sawyer_xyz/v2/sawyer_coffee_button_v2.py
@@ -1,7 +1,7 @@
 import numpy as np
 from gym.spaces import Box
 
-from metaworld.envs.env_util import get_asset_full_path
+from metaworld.envs.asset_path_utils import full_v2_path_for
 from metaworld.envs.mujoco.sawyer_xyz.sawyer_xyz_env import SawyerXYZEnv, _assert_task_is_set
 
 
@@ -48,7 +48,7 @@ class SawyerCoffeeButtonEnvV2(SawyerXYZEnv):
 
     @property
     def model_name(self):
-        return get_asset_full_path('sawyer_xyz/sawyer_coffee.xml', True)
+        return full_v2_path_for('sawyer_xyz/sawyer_coffee.xml')
 
     @_assert_task_is_set
     def step(self, action):
