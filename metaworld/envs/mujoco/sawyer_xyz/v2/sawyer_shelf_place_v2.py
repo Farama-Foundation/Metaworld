79c9a8fb634f8711e3b312712ff3feb480e070cc
Adithya Bellathur <bellathu@usc.edu>
2020-10-25 12:47:38 -0700

    Updated Reward to avoid hitting shelf

--- metaworld/envs/mujoco/sawyer_xyz/v2/sawyer_shelf_place_v2.py
+++ metaworld/envs/mujoco/sawyer_xyz/v2/sawyer_shelf_place_v2.py
@@ -165,9 +165,16 @@ class SawyerShelfPlaceEnvV2(SawyerXYZEnv):
             target = self._target_pos
 
             obj_to_target = np.linalg.norm(obj - target)
+            obj_to_out_of_shelf = np.linalg.norm(obj - (target + np.array([0, -0.1, 0])))
             tcp_to_obj = np.linalg.norm(obj - tcp)
             in_place_margin = (np.linalg.norm(self.obj_init_pos - target))
+
+
             in_place = reward_utils.tolerance(obj_to_target,
+                                        bounds=(0, _TARGET_RADIUS),
+                                        margin=in_place_margin,
+                                        sigmoid='long_tail',)
+            in_place_out_of_shelf = reward_utils.tolerance(obj_to_out_of_shelf,
                                         bounds=(0, _TARGET_RADIUS),
                                         margin=in_place_margin,
                                         sigmoid='long_tail',)
@@ -178,77 +185,10 @@ class SawyerShelfPlaceEnvV2(SawyerXYZEnv):
             reward = in_place_and_object_grasped
 
             if tcp_to_obj < 0.02 and (tcp_opened > 0) and (obj[2] - 0.01 > self.obj_init_pos[2]):
-                reward += 1. + 5. * in_place
+                if(obj[2] < (target - np.array([0,0,-0.1]))):
+                    reward += 1. + 3.5 * in_place_out_of_shelf
+                else:
+                    reward += 1 + 3.5 + 1.5 * in_place
             if obj_to_target < _TARGET_RADIUS:
                 reward = 10.
             return [reward, tcp_to_obj, tcp_opened, obj_to_target, object_grasped, in_place]
-
-
-        # objPos = obs[3:6]
-        #
-        # rightFinger, leftFinger = self._get_site_pos('rightEndEffector'), self._get_site_pos('leftEndEffector')
-        # fingerCOM  =  (rightFinger + leftFinger)/2
-        #
-        # heightTarget = self.heightTarget
-        # placingGoal = self._target_pos
-        #
-        # reachDist = np.linalg.norm(objPos - fingerCOM)
-        #
-        # placingDist = np.linalg.norm(objPos - placingGoal)
-        #
-        #
-        # def reachReward():
-        #     reachRew = -reachDist
-        #     reachDistxy = np.linalg.norm(objPos[:-1] - fingerCOM[:-1])
-        #     zRew = np.linalg.norm(fingerCOM[-1] - self.init_fingerCOM[-1])
-        #
-        #     if reachDistxy < 0.05:
-        #         reachRew = -reachDist
-        #     else:
-        #         reachRew =  -reachDistxy - 2*zRew
-        #
-        #     # incentive to close fingers when reachDist is small
-        #     if reachDist < 0.05:
-        #         reachRew = -reachDist + max(actions[-1],0)/50
-        #     return reachRew , reachDist
-        #
-        # def pickCompletionCriteria():
-        #     tolerance = 0.01
-        #     return objPos[2] >= (heightTarget- tolerance)
-        #
-        # self.pickCompleted = pickCompletionCriteria()
-        #
-        #
-        # def objDropped():
-        #     return (objPos[2] < (self.objHeight + 0.005)) and (placingDist >0.02) and (reachDist > 0.02)
-        #     # Object on the ground, far away from the goal, and from the gripper
-        #     # Can tweak the margin limits
-        #
-        # def orig_pickReward():
-        #     hScale = 100
-        #     if self.pickCompleted and not(objDropped()):
-        #         return hScale*heightTarget
-        #     elif (reachDist < 0.1) and (objPos[2]> (self.objHeight + 0.005)):
-        #         return hScale* min(heightTarget, objPos[2])
-        #     else:
-        #         return 0
-        #
-        # def placeReward():
-        #     c1 = 1000
-        #     c2 = 0.01
-        #     c3 = 0.001
-        #     cond = self.pickCompleted and (reachDist < 0.1) and not(objDropped())
-        #     if cond:
-        #         placeRew = 1000*(self.maxPlacingDist - placingDist) + c1*(np.exp(-(placingDist**2)/c2) + np.exp(-(placingDist**2)/c3))
-        #         placeRew = max(placeRew,0)
-        #         return [placeRew , placingDist]
-        #     else:
-        #         return [0 , placingDist]
-        #
-        # reachRew, reachDist = reachReward()
-        # pickRew = orig_pickReward()
-        # placeRew , placingDist = placeReward()
-        # assert ((placeRew >=0) and (pickRew>=0))
-        # reward = reachRew + pickRew + placeRew
-        #
-        # return [reward, reachRew, reachDist, pickRew, placeRew, placingDist]
