5fdf5edf6ea7679a08d8a9c0ffd1c23721de9331
Adithya Bellathur <bellathu@usc.edu>
2020-10-26 11:10:44 -0700

    New Reward Part 11

--- metaworld/envs/mujoco/sawyer_xyz/v2/sawyer_shelf_place_v2.py
+++ metaworld/envs/mujoco/sawyer_xyz/v2/sawyer_shelf_place_v2.py
@@ -177,11 +177,17 @@ class SawyerShelfPlaceEnvV2(SawyerXYZEnv):
         reward = reward_utils.hamacher_product(object_grasped, in_place)
 
         if (0.0 < obj[2] < 0.275 and
-                (target[0]+0.15 < obj[0] < target[0]+0.15) and
-                ( (target[1] - 4*_TARGET_RADIUS) < obj[1] < target[1]) ):
-            z_scaling = (0.275 - obj[2])/0.275
-            y_scaling = (obj[1] - (target[1] - 4*_TARGET_RADIUS)) / (4*_TARGET_RADIUS)
-            in_place -= reward_utils.hamacher_product(y_scaling, z_scaling)
+                (target[0]-0.15 < obj[0] < target[0]+0.15) and
+                ( (target[1] - 6*_TARGET_RADIUS) < obj[1] < target[1] - 2*_TARGET_RADIUS) ):
+            z_scaling = 1-(0.275 - obj[2])/0.275
+            y_scaling = 1-(obj[1] - (target[1] - 6*_TARGET_RADIUS)) / (4*_TARGET_RADIUS)
+            # bound_loss = min(1, 2*reward_utils.hamacher_product(y_scaling, z_scaling))
+            bound_loss = reward_utils.hamacher_product(y_scaling, z_scaling)
+            print("HERE :: {}".format(bound_loss))
+            # print("OLD :: {} - NEW :: {}".format(in_place, in_place-bound_loss))
+            in_place *= bound_loss
+            assert 0 <= in_place <= 1
+
 
         if tcp_to_obj < 0.025 and (tcp_opened > 0) and (obj[2] - 0.01 > self.obj_init_pos[2]):
                 reward += 1. + 5. * in_place
