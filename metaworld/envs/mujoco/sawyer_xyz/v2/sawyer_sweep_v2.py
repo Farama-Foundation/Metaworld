0eed22184750ddab03f23002e16c8f73e3036f0c
Adithya Bellathur <bellathu@usc.edu>
2020-11-01 00:31:06 -0700

    Make Sweep Even More Similar to Original

--- metaworld/envs/mujoco/sawyer_xyz/v2/sawyer_sweep_v2.py
+++ metaworld/envs/mujoco/sawyer_xyz/v2/sawyer_sweep_v2.py
@@ -128,13 +128,12 @@ class SawyerSweepEnvV2(SawyerXYZEnv):
                                     sigmoid='long_tail',)
 
         object_grasped = self._gripper_caging_reward(action, obj, self.OBJ_RADIUS)
-        # in_place_and_object_grasped = reward_utils.hamacher_product(object_grasped,
-        #                                                             in_place)
-        # reward = in_place_and_object_grasped
-        reward = 2 * object_grasped
+        in_place_and_object_grasped = reward_utils.hamacher_product(object_grasped,
+                                                                    in_place)
+        reward = in_place_and_object_grasped
 
         if tcp_to_obj < 0.02 and (tcp_opened > 0):
-            reward += 5. * in_place
+            reward += 1 + 5. * in_place
         if obj_to_target < _TARGET_RADIUS:
             reward = 10.
         return [reward, tcp_to_obj, tcp_opened, obj_to_target, object_grasped, in_place]
