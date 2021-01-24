90c0693a3859e8bfde29dcc916119fc1d4849c59
Adithya Bellathur <bellathu@usc.edu>
2021-01-24 15:21:15 -0800

    Attempt 16: Last attempt

--- metaworld/envs/mujoco/sawyer_xyz/v2/sawyer_stick_pull_v2.py
+++ metaworld/envs/mujoco/sawyer_xyz/v2/sawyer_stick_pull_v2.py
@@ -182,7 +182,7 @@ class SawyerStickPullEnvV2(SawyerXYZEnv):
         handle_to_target = np.linalg.norm(handle - target)
 
         # stick_to_container = np.linalg.norm(end_of_stick[:2] - container[:2])
-        yz_scaling = np.array([1., 1., 3.])
+        yz_scaling = np.array([1., 1., 2.])
         stick_to_container = np.linalg.norm((stick - container) * yz_scaling)
         stick_in_place_margin = (np.linalg.norm(
             (self.stick_init_pos - container_init_pos) * yz_scaling
