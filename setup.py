dc776b7073276cd6bb99f09974e1f7ed884b5c5d
Sidd Karamcheti <skaramcheti@cs.stanford.edu>
2021-11-30 11:50:17 -0800

    Update setup.py with new, open version of Mujoco

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 required = [
     # Please keep alphabetized
     'gym>=0.15.4',
-    'mujoco-py<2.1,>=2.0',
+    'mujoco-py<2.2,>=2.1',
     'numpy>=1.18',
 ]
 
