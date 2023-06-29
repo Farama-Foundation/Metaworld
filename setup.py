eda06dce6304efeff720245c9fadab914dd2cc5a
elliottower <elliot@elliottower.com>
2023-06-29 15:53:38 -0400

    Change setup to not install deps, hardcoded version number to test

--- setup.py
+++ setup.py
@@ -1,34 +1,21 @@
-from setuptools import find_packages, setup
+"""Sets up the project."""
+
+# import pathlib
+
+from setuptools import setup
+
+# CWD = pathlib.Path(__file__).absolute().parent
+#
+#
+# def get_version():
+#     """Gets the metaworld version."""
+#     path = CWD / "metaworld" / "__init__.py"
+#     content = path.read_text()
+#
+#     for line in content.splitlines():
+#         if line.startswith("__version__"):
+#             return line.strip().split()[-1].strip().strip('"')
+#     raise RuntimeError("bad version data in __init__.py")
 
 
-# Required dependencies
-required = [
-    # Please keep alphabetized
-    "gym>=0.15.4",
-    "mujoco-py<2.2,>=2.0",
-    "numpy>=1.18",
-    "scipy>=1.4.1",
-]
-
-
-# Development dependencies
-extras = dict()
-extras["dev"] = [
-    # Please keep alphabetized
-    "ipdb",
-    "memory_profiler",
-    "pylint",
-    "pyquaternion==0.9.5",
-    "pytest>=4.4.0",  # Required for pytest-xdist
-    "pytest-xdist",
-]
-
-
-setup(
-    name="metaworld",
-    version="0.1.0",
-    packages=find_packages(),
-    include_package_data=True,
-    install_requires=required,
-    extras_require=extras,
-)
+setup(name="metaworld", version="2.0.0")
