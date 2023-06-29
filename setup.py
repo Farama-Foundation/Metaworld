59d2b5995c0490226eddf976cecc5f255cd27d68
elliottower <elliot@elliottower.com>
2023-06-29 16:14:38 -0400

    Test adding long description from gymnasium, dynamic version w/ setup

--- setup.py
+++ setup.py
@@ -1,21 +1,36 @@
 """Sets up the project."""
 
-# import pathlib
+import pathlib
 
 from setuptools import setup
 
-# CWD = pathlib.Path(__file__).absolute().parent
-#
-#
-# def get_version():
-#     """Gets the metaworld version."""
-#     path = CWD / "metaworld" / "__init__.py"
-#     content = path.read_text()
-#
-#     for line in content.splitlines():
-#         if line.startswith("__version__"):
-#             return line.strip().split()[-1].strip().strip('"')
-#     raise RuntimeError("bad version data in __init__.py")
+CWD = pathlib.Path(__file__).absolute().parent
 
 
-setup(name="metaworld", version="2.0.0")
+def get_version():
+    """Gets the metaworld version."""
+    path = CWD / "metaworld" / "__init__.py"
+    content = path.read_text()
+
+    for line in content.splitlines():
+        if line.startswith("__version__"):
+            return line.strip().split()[-1].strip().strip('"')
+    raise RuntimeError("bad version data in __init__.py")
+
+
+def get_description():
+    """Gets the description from the readme."""
+    with open("README.md") as fh:
+        long_description = ""
+        header_count = 0
+        for line in fh:
+            if line.startswith("##"):
+                header_count += 1
+            if header_count < 2:
+                long_description += line
+            else:
+                break
+    return long_description
+
+
+setup(name="gymnasium", version=get_version(), long_description=get_description())
