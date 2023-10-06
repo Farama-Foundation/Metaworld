6e034fca481e6303de7df972c732eee7c188baa8
Reggie McLean <reginald.mclean@ryerson.ca>
2023-10-06 13:13:25 -0400

    Changed from equal to max steps, to greater than or equal to max steps

--- metaworld/envs/mujoco/sawyer_xyz/sawyer_xyz_env.py
+++ metaworld/envs/mujoco/sawyer_xyz/sawyer_xyz_env.py
@@ -458,7 +458,7 @@ class SawyerXYZEnv(SawyerMocapBase, EzPickle):
     def step(self, action):
         assert len(action) == 4, f"Actions should be size 4, got {len(action)}"
         self.set_xyz_action(action[:3])
-        if self.curr_path_length == self.max_path_length:
+        if self.curr_path_length >= self.max_path_length:
             raise ValueError("You must reset the env manually once truncate==True")
         self.do_simulation([action[-1], -action[-1]], n_frames=self.frame_skip)
         self.curr_path_length += 1
