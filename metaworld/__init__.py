852ea8106e46a4a348ae9b24664cd1ce73e4c8f6
Reggie McLean <reginald.mclean@ryerson.ca>
2024-08-29 21:53:46 -0400

    pre-commit

--- metaworld/__init__.py
+++ metaworld/__init__.py
@@ -31,9 +31,6 @@ from metaworld.wrappers import (
     PseudoRandomTaskSelectWrapper,
     RandomTaskSelectWrapper,
 )
-from metaworld.types import Task
-
-from .sawyer_xyz_env import SawyerXYZEnv
 
 
 class MetaWorldEnv(abc.ABC):
