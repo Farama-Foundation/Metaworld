b32454e5525d86a5d64aa13c0a2cbff906edd574
Reggie McLean <reginald.mclean@ryerson.ca>
2024-11-06 21:17:06 +0100

    updating evaluation tests for gym.make changes

--- tests/metaworld/test_evaluation.py
+++ tests/metaworld/test_evaluation.py
@@ -82,7 +82,10 @@ def test_evaluation():
     random.seed(SEED)
     np.random.seed(SEED)
     envs = gym.make_vec(
-        "Meta-World/MT50-async", seed=SEED, max_episode_steps=max_episode_steps
+        "Meta-World/MT50",
+        seed=SEED,
+        vector_strategy="async",
+        max_episode_steps=max_episode_steps,
     )
     agent = ScriptedPolicyAgent(envs)
     mean_success_rate, mean_returns, success_rate_per_task = evaluation.evaluation(
@@ -109,8 +112,9 @@ def test_metalearning_evaluation(benchmark):
     random.seed(SEED)
     np.random.seed(SEED)
     envs = gym.make_vec(
-        f"Meta-World/{benchmark}-test-async",
+        f"Meta-World/{benchmark}-test",
         seed=SEED,
+        vector_strategy="async",
         meta_batch_size=meta_batch_size,
         max_episode_steps=max_episode_steps,
     )
